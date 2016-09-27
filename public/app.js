62fc988d7e1d116a6c15d764449c404fd218c779
Robbie Campbell <rcampbell182@gmail.com>
2016-09-27 21:10:53 +0100

    initial commit

---
+++ public/app.js
@@ -0,0 +1,28 @@
+var initialize = function() {
+  var mapContainer = document.querySelector("#map");
+  var centre = { lat: 55.94716, lng: -3.20198 };
+  var map = new Map(mapContainer, centre, 10);
+  var codeClan = map.addMarker(centre);
+  map.addClickEvent();
+
+  var infoWindowText = "<p>Window info test</p>"
+  var infoWindow = map.addInfoWindow( infoWindowText );
+  map.addListenerForInfoWindow(infoWindow, codeClan);
+
+  var button = createAndAddButton();
+  button.onclick = map.setCenter(new google.maps.LatLng(57.477773, -4.224721));
+}
+
+var createAndAddButton = function() {
+  var button = document.createElement( "button" );
+  button.classList.add( "moveTo" );
+  button.innerText = "Take me to the land of rubber bumpers";
+  var pButton = document.createElement( "p" );
+  pButton.classList.add( "buttonDiv" );
+  pButton.appendChild( button );
+  var body = document.querySelector( "body" );
+  body.appendChild( pButton );
+  return button;
+}
+
+window.onload = initialize;
